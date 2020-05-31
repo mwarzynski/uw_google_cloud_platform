ab94c8feac973e3269e59702cb98b43a0732cdb5
Mateusz Warzyński <mathew.warzynski@gmail.com>
2020-05-31 21:39:54 +0200

    Add enabling APIs to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,10 @@ export PROJECT_ID=$(gcloud config get-value project)
 #gcloud services enable cloudfunctions.googleapis.com
 #gcloud services enable pubsub.googleapis.com
 #gcloud services enable vision.googleapis.com
+gcloud services enable appengine.googleapis.com
+gcloud services enable storage-api.googleapis.com
+gcloud services enable logging.googleapis.com
+gcloud services enable clouderrorreporting.googleapis.com
 
 # Creating a bucket for Cloud Functions
 export BUCKET_GCF=$PROJECT_ID-gcf
@@ -23,9 +27,13 @@ export BUCKET_IMAGES_SCALED=$PROJECT_ID-images-scaled
 export TOPIC_IMAGE_WAS_SCALED=image-was-scaled
 #gcloud pubsub topics create $TOPIC_IMAGE_WAS_SCALED
 
+# Create Firebase Datastore
+# https://console.cloud.google.com/firestore/welcome?project=$PROJECT_ID
+# It's not possible to do so using `gcloud`.
+
 # Deployment of Cloud Function
 #(cd gcf_1; ./deploy.sh)
-(cd gcf_2; ./deploy.sh)
+#(cd gcf_2; ./deploy.sh)
 #(cd gcf_3; ./deploy.sh)
 
 # # Deploy service 'app' to Cloud Run.
