7c1146973974c08ff60da0b2c0ed0dd09e9459fe
Mateusz Warzyński <mathew.warzynski@gmail.com>
2020-06-06 21:35:25 +0200

    Simplify GCF3

--- gcf_3/deploy.sh
+++ gcf_3/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 gcloud functions deploy gcf3 \
-    --trigger-topic=$TOPIC_IMAGE_WAS_SCALED \
-    --set-env-vars \
-      BUCKET_IMAGES=$BUCKET_IMAGES,BUCKET_IMAGES_SCALED=$BUCKET_IMAGES_SCALED,SENDGRID_API_KEY=$SENDGRID_API_KEY \
+    --trigger-topic=$TOPIC_EMAILS_TO_SEND \
+    --set-env-vars SENDGRID_API_KEY=$SENDGRID_API_KEY \
     --runtime python37
--- gcf_3/main.py
+++ gcf_3/main.py
@@ -15,66 +15,18 @@
 import base64
 import json
 import os
-import datetime
 
-from google.cloud import storage
 import sendgrid
 
-bucket_images = os.getenv("BUCKET_IMAGES")
-bucket_images_scaled = os.getenv("BUCKET_IMAGES_SCALED")
-
-sendgrid_api_key = os.getenv("SENDGRID_API_KEY")
-sg = sendgrid.SendGridAPIClient(api_key=sendgrid_api_key)
-
-storage_client = storage.Client()
+sg = sendgrid.SendGridAPIClient(api_key=os.getenv("SENDGRID_API_KEY"))
 
 
-def generate_download_signed_url_v4(bucket_name, blob_name):
-    """Generates a v4 signed URL for downloading a blob.
-
-    Note that this method requires a service account key file. You can not use
-    this if you are using Application Default Credentials from Google Compute
-    Engine or from the Google Cloud SDK.
-    """
-
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(blob_name)
-    url = blob.generate_signed_url(
-        version="v4",
-        expiration=datetime.timedelta(minutes=15),
-        method="GET",
-    )
-    return url
-
-
-def gcf3(event, context):
-    """Background Cloud Function to be triggered by Pub/Sub.
-    Args:
-         event (dict):  The dictionary with data specific to this type of
-         event. The `data` field contains the PubsubMessage message. The
-         `attributes` field will contain custom attributes if there are any.
-         context (google.cloud.functions.Context): The Cloud Functions event
-         metadata. The `event_id` field contains the Pub/Sub message ID. The
-         `timestamp` field contains the publish time.
-    """
-    try:
-        event_raw_data = base64.b64decode(event['data']).decode('utf-8')
-        event = json.loads(event_raw_data)
-
-        blob_name = event['blob_name']
-
-        # image_original = generate_download_signed_url_v4(bucket_images, blob_name)
-        # image_transformed = generate_download_signed_url_v4(bucket_images_scaled, blob_name)
-        image_original = blob_name
-        image_transformed = blob_name
-        image_text = event['image_text']
-
-        message = sendgrid.Mail(
-            from_email="noreply@mimuwgcpproject.pl",
-            to_emails=event['email'],
-            subject="Image has been processed!",
-            html_content=f"Image original: {image_original}\nImage transformed: {image_transformed}\n\n{image_text}"
-        )
-        sg.send(message)
-    except Exception as e:
-        print(e)
+def gcf3(event, _):
+    event_raw_data = base64.b64decode(event['data']).decode('utf-8')
+    event = json.loads(event_raw_data)
+    sg.send(sendgrid.Mail(
+        from_email=event["sender"],
+        to_emails=event["recipient"],
+        subject=event["title"],
+        html_content=event["html_content"]
+    ))
