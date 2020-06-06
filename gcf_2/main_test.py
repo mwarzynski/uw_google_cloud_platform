08bec54f900811bee0d262fd46b67f2ea1d7a183
Mateusz Warzyński <mathew.warzynski@gmail.com>
2020-06-07 00:19:25 +0200

    gcf2: Add unit tests

--- gcf_2/main_test.py
+++ gcf_2/main_test.py
@@ -13,98 +13,67 @@
 # limitations under the License.
 
 from collections import UserDict
-import uuid
-
 from mock import MagicMock, patch
 
 import main
 
 
-# @patch('main.__blur_image')
-# @patch('main.vision_client')
-# @patch('main.storage_client')
-# def test_process_offensive_image(
-#   storage_client,
-#   vision_client,
-#   __blur_image,
-#   capsys):
-#     result = UserDict()
-#     result.safe_search_annotation = UserDict()
-#     result.safe_search_annotation.adult = 5
-#     result.safe_search_annotation.violence = 5
-#     vision_client.safe_search_detection = MagicMock(return_value=result)
-#
-#     filename = str(uuid.uuid4())
-#     data = {
-#       'bucket': 'my-bucket',
-#       'name': filename
-#     }
-#
-#     main.blur_offensive_images(data, None)
-#
-#     out, _ = capsys.readouterr()
-#     assert 'Analyzing %s.' % filename in out
-#     assert 'The image %s was detected as inappropriate.' % filename in out
-#     assert main.__blur_image.called
-#
-#
-# @patch('main.__blur_image')
-# @patch('main.vision_client')
-# @patch('main.storage_client')
-# def test_process_safe_image(
-#   storage_client,
-#   vision_client,
-#   __blur_image,
-#   capsys):
-#     result = UserDict()
-#     result.safe_search_annotation = UserDict()
-#     result.safe_search_annotation.adult = 1
-#     result.safe_search_annotation.violence = 1
-#     vision_client.safe_search_detection = MagicMock(return_value=result)
-#
-#     filename = str(uuid.uuid4())
-#     data = {
-#       'bucket': 'my-bucket',
-#       'name': filename
-#     }
-#
-#     main.blur_offensive_images(data, None)
-#
-#     out, _ = capsys.readouterr()
-#
-#     assert 'Analyzing %s.' % filename in out
-#     assert 'The image %s was detected as OK.' % filename in out
-#     assert __blur_image.called is False
-#
-#
-# @patch('main.os')
-# @patch('main.Image')
-# @patch('main.storage_client')
-# def test_blur_image(storage_client, image_mock, os_mock, capsys):
-#     filename = str(uuid.uuid4())
-#     blur_bucket = 'blurred-bucket-' + str(uuid.uuid4())
-#
-#     os_mock.remove = MagicMock()
-#     os_mock.path = MagicMock()
-#     os_mock.path.basename = MagicMock(side_effect=(lambda x: x))
-#
-#     os_mock.getenv = MagicMock(return_value=blur_bucket)
-#
-#     image_mock.return_value = image_mock
-#     image_mock.__enter__.return_value = image_mock
-#
-#     blob = UserDict()
-#     blob.name = filename
-#     blob.bucket = UserDict()
-#     blob.download_to_filename = MagicMock()
-#     blob.upload_from_filename = MagicMock()
-#
-#     main.__blur_image(blob)
-#
-#     out, _ = capsys.readouterr()
-#
-#     assert f'Image {filename} was downloaded to' in out
-#     assert f'Image {filename} was blurred.' in out
-#     assert f'Blurred image uploaded to: gs://{blur_bucket}/{filename}' in out
-#     assert os_mock.remove.called
-#     assert image_mock.resize.called
+@patch('main.publisher')
+@patch('main.vision_client')
+@patch('main.datastore')
+def test_image_processed_with_text(
+    datastore,
+    vision_client,
+    publisher):
+  # Define basic information which is provided as arguments.
+  image_id = "123456789"
+  image_name = f"{image_id}.jpg"
+  # Define response from the Visual API client.
+  visual_text_annotation = UserDict()
+  visual_text_annotation.description = "Text visible in the image."
+  visual_text_detection_response = UserDict()
+  visual_text_detection_response.text_annotations = [visual_text_annotation]
+  vision_client.text_detection = MagicMock(return_value=visual_text_detection_response)
+  # Define Datastore implementation.
+  datastore.get_image_by_id = MagicMock(return_value={"email": "tester@google.com", "filename": image_name})
+  datastore.update_image_text = MagicMock()
+  # Define Pub/Sub implementation.
+  publisher.publish = MagicMock()
+
+  main.gcf2({"name": image_name}, None)
+
+  assert vision_client.text_detection.called is True
+  assert datastore.update_image_text.called is True
+  assert publisher.publish.called is True
+  publish_args = publisher.publish.call_args.args
+  assert publish_args[1] == b'{"recipient": "tester@google.com", "sender": "noreply@gcplecturesproject.com", "title": "123456789.jpg has been processed!", "html_content": "Image original: 123456789.jpg<br/>Image transformed: 123456789.jpg<br/><br/>Text visible in the image."}'
+
+
+@patch('main.publisher')
+@patch('main.vision_client')
+@patch('main.datastore')
+def test_image_processed_with_no_text(
+    datastore,
+    vision_client,
+    publisher,
+    capsys):
+  # Define basic information which is provided as arguments.
+  image_id = "123456789"
+  image_name = f"{image_id}.jpg"
+  # Define response from the Visual API client.
+  visual_text_detection_response = UserDict()
+  visual_text_detection_response.text_annotations = []
+  vision_client.text_detection = MagicMock(return_value=visual_text_detection_response)
+  # Define Datastore implementation.
+  datastore.get_image_by_id = MagicMock()
+  datastore.update_image_text = MagicMock()
+  # Define Pub/Sub implementation.
+  publisher.publish = MagicMock()
+
+  main.gcf2({"name": image_name}, None)
+
+  assert vision_client.text_detection.called is True
+  assert datastore.update_image_text.called is False
+  assert publisher.publish.called is False
+  out, _ = capsys.readouterr()
+  assert 'No annotations received from Visual API' in out
