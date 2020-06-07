50e66d1373a5792395ded7e835c355a4d3ae521f
Mateusz Warzyński <mathew.warzynski@gmail.com>
2020-06-07 14:14:43 +0200

    Changes

--- gcf_1/deploy.sh
+++ gcf_1/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 gcloud functions deploy gcf1 \
-    --trigger-bucket=$BUCKET_IMAGES \
+    --trigger-bucket=${_BUCKET_IMAGES} \
     --set-env-vars \
-        BUCKET_IMAGES_SCALED=$BUCKET_IMAGES_SCALED \
+        BUCKET_IMAGES_SCALED=${_BUCKET_IMAGES_SCALED} \
     --runtime python37
 
